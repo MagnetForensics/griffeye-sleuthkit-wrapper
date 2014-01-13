6780df74253884e2617440aa20c13d3845f7829f
Josef Eklann <NETCLEANTECH\Josef.Eklann>
2014-01-13 13:28:31 +0100

    Fixed bug with file metadata marshalling

--- Source/Sleuthkit Wrapper/src/Structures.cs
+++ Source/Sleuthkit Wrapper/src/Structures.cs
@@ -66,7 +66,7 @@ namespace SleuthKit
             get
             {
                 FileMetadata? ret = null;
-                if (ptr_fsinfo != IntPtr.Zero)
+                if (ptr_meta != IntPtr.Zero)
                 {
                     ret = (FileMetadata)Marshal.PtrToStructure(ptr_meta, typeof(FileMetadata));
                 }
