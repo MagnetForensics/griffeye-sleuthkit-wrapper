ee6ca29f431bf18f76211fe5c2c8a47a19beaf1c
Josef Eklann <josef.eklann@netclean.com>
2015-09-30 11:00:10 +0200

    Removed test based on crashing image (crash due to libtsk/libewf)

--- Source/Sleuthkit Wrapper/unit-tests/FileSystemTest.cs
+++ Source/Sleuthkit Wrapper/unit-tests/FileSystemTest.cs
@@ -1,6 +1,5 @@
 ﻿using NUnit.Framework;
 using SleuthKit;
-using SleuthKit.Structs;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -19,28 +18,6 @@ namespace SleuthkitSharp_UnitTests
 
             Assert.AreEqual(1, fileSystems.Count);
             Assert.AreEqual("ExFAT", fileSystems.First().Type.ToString());
-        }
-
-        [Test]
-        public void TestDSS3()
-        {
-            String imagePath = @"I:\TestSets\Mixed\DATASET_SAFE_III\Images and exports\DSIII.E01";
-            DiskImage image = new DiskImage(new System.IO.FileInfo(imagePath));
-
-            foreach (FileSystem fs in image.GetFileSystems())
-            {
-                fs.WalkDirectories(DirWalkCallback);
-            }
-
-            Assert.GreaterOrEqual(1000, fileCount);
-        }
-
-        private int fileCount = 0;
-
-        private WalkReturnEnum DirWalkCallback(ref TSK_FS_FILE file, string path, IntPtr some_ptr)
-        {
-            fileCount += file.AppearsValid ? 1 : 0;
-            return WalkReturnEnum.Continue;
         }
     }
 }
