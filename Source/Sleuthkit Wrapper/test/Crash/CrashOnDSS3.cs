2f638f35fb70d5abbd4b174c34e3355780e5e9b1
Josef Eklann <josef.eklann@netclean.com>
2015-09-30 09:26:01 +0200

    Debugging the crash

---
+++ Source/Sleuthkit Wrapper/test/Crash/CrashOnDSS3.cs
@@ -0,0 +1,34 @@
+﻿using SleuthKit;
+using SleuthKit.Structs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Org.SleuthKit.Crash
+{
+    public class CrashOnDSS3
+    {
+        public static void Main(String[] args)
+        {
+            String imagePath = @"I:\TestSets\Mixed\DATASET_SAFE_III\Images and exports\DSIII.E01";
+            DiskImage image = new DiskImage(new System.IO.FileInfo(imagePath));
+
+            List<FileSystem> fileSystems = image.GetFileSystems().ToList();
+            foreach (FileSystem fs in image.GetFileSystems())
+            {
+                fs.WalkDirectories(DirWalkCallback);
+            }
+
+            Console.WriteLine(String.Format("Found {0} files.", fileCount));
+            Console.ReadLine();
+        }
+
+        private static int fileCount = 0;
+
+        private static WalkReturnEnum DirWalkCallback(ref TSK_FS_FILE file, string path, IntPtr some_ptr)
+        {
+            fileCount += file.AppearsValid ? 1 : 0;
+            return WalkReturnEnum.Continue;
+        }
+    }
+}
--- Source/Sleuthkit Wrapper/unit-tests/FileSystemTest.cs
+++ Source/Sleuthkit Wrapper/unit-tests/FileSystemTest.cs
@@ -1,5 +1,6 @@
 ﻿using NUnit.Framework;
 using SleuthKit;
+using SleuthKit.Structs;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,7 +8,7 @@ using System.Linq;
 namespace SleuthkitSharp_UnitTests
 {
     [TestFixture]
-    class FileSystemTest
+    internal class FileSystemTest
     {
         [Test]
         public void TestExFAT()
@@ -18,6 +19,28 @@ namespace SleuthkitSharp_UnitTests
 
             Assert.AreEqual(1, fileSystems.Count);
             Assert.AreEqual("ExFAT", fileSystems.First().Type.ToString());
+        }
+
+        [Test]
+        public void TestDSS3()
+        {
+            String imagePath = @"I:\TestSets\Mixed\DATASET_SAFE_III\Images and exports\DSIII.E01";
+            DiskImage image = new DiskImage(new System.IO.FileInfo(imagePath));
+
+            foreach (FileSystem fs in image.GetFileSystems())
+            {
+                fs.WalkDirectories(DirWalkCallback);
+            }
+
+            Assert.GreaterOrEqual(1000, fileCount);
+        }
+
+        private int fileCount = 0;
+
+        private WalkReturnEnum DirWalkCallback(ref TSK_FS_FILE file, string path, IntPtr some_ptr)
+        {
+            fileCount += file.AppearsValid ? 1 : 0;
+            return WalkReturnEnum.Continue;
         }
     }
 }
